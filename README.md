d83aa6a4f8cf076b0d835d49847dd759f465d05d
Gabriel <gabriel@Gabriels-MacBook-Air.local>
2018-12-02 01:00:33 -0400

    Basic blockchain skeleton ready

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 Just a collection of blockchain related code and experiments.
 
 ### Contents:
-    - blockchains: simple implementation of blockchains in many programming languages.
-        - Python: This implementation is based on [Building a Blockchain](https://medium.com/p/117428612f46)
+ - blockchains: simple implementation of blockchains in many programming languages.
+   - Python: This implementation is based on [Building a Blockchain](https://medium.com/p/117428612f46)
--- blockchains/blockchain.py
+++ blockchains/blockchain.py
@@ -1,3 +1,7 @@
+from time import time
+import hashlib
+import json
+
 
 class Blockchain:
 
@@ -5,20 +9,62 @@ class Blockchain:
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        # Adds a new block to the blockchain.
-        pass
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
 
-    def new_transaction(self):
-        # Adds a new transaction to a block.
-        pass
+    def new_block(self, proof, previous_hash=None):
+        """
+        Adds a new block to the blockchain.
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # Reset the current list of transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Adds a new transaction to a block.
+
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
+
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
 
     @property
     def last_block(self):
         # returns the last block.
-        pass
+        return self.chain[-1]
 
     @staticmethod
     def hash(block):
-        # Generates a block's hash
-        pass
+        """
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
+
+        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
