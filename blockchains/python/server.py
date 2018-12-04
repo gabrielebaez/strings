2323090a93306abf8e117aeaf61742fa37313068
Gabriel <gabriel@mctekk.com>
2018-12-04 18:33:56 -0400

    python chain fully implemented

--- blockchains/python/server.py
+++ blockchains/python/server.py
@@ -8,7 +8,7 @@ import json
 app = Flask(__name__)
 
 # Generate a globally unique address for this node
-node_identifier = str(uuid4()).replace('-','')
+node_identifier = str(uuid4()).replace('-', '')
 
 # Instantiate the blockchain
 blockchain = Blockchain()
@@ -76,6 +76,46 @@ def full_chain():
     }
 
     return jsonify(response), 200
+
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return 'ERROR: Please supply a valid list of nodes', 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+
+    return jsonify(response)
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+
+    else:
+
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+
+    return jsonify(response), 400
 
 
 if __name__ == '__main__':
