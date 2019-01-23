91a9f3c956292cdac9138c8e43b9daf6f8887657
Gabrielebaez <gabrielevang@gmail.com>
2019-01-23 11:51:47 -0400

    Roles implemented

--- paxos/roles.py
+++ paxos/roles.py
@@ -28,7 +28,7 @@ class Acceptor(Role):
     def __init__(self, node):
         super(Acceptor, self).__init__(node)
         self.ballot_num = NULL_BALLOT
-        self.accepted_proposals = {} # {slot: (ballot_num, proposal)}
+        self.accepted_proposals = {}  # {slot: (ballot_num, proposal)}
 
     def do_prepare(self, sender, ballot_num):
         if ballot_num > self.ballot_num:
@@ -37,8 +37,8 @@ class Acceptor(Role):
             self.node.send([self.node.address], Accepting(leader=sender))
 
         self.node.send([sender], Promise(
-            ballot_num = self.ballot_num,
-            accepted_proposals = self.accepted_proposals))
+            ballot_num=self.ballot_num,
+            accepted_proposals=self.accepted_proposals))
 
     def do_accept(self, sender, ballot_num, slot, proposal):
         if ballot_num >= self.ballot_num:
@@ -48,8 +48,8 @@ class Acceptor(Role):
                 acc[slot] = (ballot_num, proposal)
 
         self.node.send([sender], Accepted(
-            slot = slot,
-            ballot_num = self.ballot_num))
+            slot=slot,
+            ballot_num=self.ballot_num))
 
 
 class Replica(Role):
@@ -63,7 +63,7 @@ class Replica(Role):
         self.decisions = decisions
         self.peers = peers
         self.proposals = {}
-        self.next_slot = slot # next slot num for a proposal (may lead slot)
+        self.next_slot = slot  # next slot num for a proposal (may lead slot)
         self.latest_leader = None
         self.latest_leader_timeout = None
 
@@ -168,29 +168,248 @@ class Replica(Role):
 
 class Leader(Role):
     """ lead rounds of the Multi-Paxos algorithm """
-    pass
+
+    def __init__(self, node, peers, commander_cls=Commander, scout_cls=Scout):
+        super(Leader, self).__init__(node)
+        self.ballot_num = Ballot(0, node.address)
+        self.active = False
+        self.proposals = {}
+        self.commander_cls = commander_cls
+        self.scout_cls = scout_cls
+        self.scouting = False
+        self.peers = peers
+
+    def start(self):
+        # reminder others we're active before LEADER_TIMEOUT expires
+        def active():
+            if self.active:
+                self.node.send(self.peers, Active())
+            self.set_timer(LEADER_TIMEOUT / 2.0, active)
+
+        active()
+
+    def spawn_scout(self):
+        assert not self.scouting
+        self.scouting = True
+        self.scout_cls(self.node, self.ballot_num, self.peers).start()
+
+    def do_adopted(self, sender, ballot_num, accepted_proposals):
+        self.scouting = False
+        self.proposals.update(accepted_proposals)
+        # note that we don't re-spawn commanders here; if there are undecided
+        # proposals, the replicas will re-propose
+        self.logger.info("leader becoming active")
+        self.active = True
+
+    def spawn_commander(self, ballot_num, slot):
+        proposal = self.proposals[slot]
+        self.commander_cls(self.node, ballot_num, slot, proposal, self.peers).start()
+
+    def do_areempted(self, sender, slot, preempted_by):
+        if not slot:  # from the scout
+            self.scouting = False
+        self.logger.info("leader preempted by %s", preempted_by.leader)
+        self.active = False
+        self.ballot_num = Ballot((preempted_by or self.ballot_num).n + 1,
+                                 self.ballot_num.leader)
+
+    def do_aropose(self, sender, slot, proposal):
+        if slot not in self.proposals:
+            if self.active:
+                self.proposals[slot] = proposal
+                self.logger.info("spawning commander for slot %d" % (slot,))
+                self.spawn_commander(self.ballot_num, slot)
+            else:
+                if not self.scouting:
+                    self.logger.info("got PROPOSE when not active - scouting")
+                    self.spawn_scout()
+                else:
+                    self.logger.info("got PROPOSE while scouting; ignored")
+        else:
+            self.logger.info("got PROPOSE for a slot already being proposed")
 
 
 class Scout(Role):
     """ perform the Prepare/Promise portion of the Multi-Paxos algorithm for a leader """
-    pass
+
+    def __init__(self, node, ballot_num, peers):
+        super(Scout, self).__init__(node)
+        self.ballot_num = ballot_num
+        self.accepted_proposals = {}
+        self.acceptors = set([])
+        self.peers = peers
+        self.quorum = len(peers) / 2 + 1
+        self.retransmit_timer = None
+
+    def start(self):
+        self.logger.info("scout starting")
+        self.send_prepare()
+
+    def send_prepare(self):
+        self.node.send(self.peers, Prepare(ballot_num=self.ballot_num))
+        self.retransmit_timer = self.set_timer(PREPARE_RETRANSMIT, self.send_prepare)
+
+    def update_accepted(self, accepted_proposals):
+        acc = self.accepted_proposals
+        for slot, (ballot_num, proposal) in accepted_proposals.iteritems():
+            if slot not in acc or acc[slot][0] < ballot_num:
+                acc[slot] = (ballot_num, proposal)
+
+    def do_promise(self, sender, ballot_num, accepted_proposals):
+        if ballot_num == self.ballot_num:
+            self.logger.info("got matching promise; need %d" % self.quorum)
+            self.update_accepted(accepted_proposals)
+            self.acceptors.add(sender)
+            if len(self.acceptors) >= self.quorum:
+                # strip the ballot numbers from self.accepted_proposals, now that it
+                # represents a majority
+                accepted_proposals = \
+                    dict((s, p) for s, (b, p) in self.accepted_proposals.iteritems())
+                # We're adopted; note that this does *not* mean that no other
+                # leader is active.  # Any such conflicts will be handled by the
+                # commanders.
+                self.node.send([self.node.address],
+                               Adopted(ballot_num=ballot_num,
+                                       accepted_proposals=accepted_proposals))
+                self.stop()
+        else:
+            # this acceptor has promised another leader a higher ballot number,
+            # so we've lost
+            self.node.send([self.node.address],
+                           Preempted(slot=None, preempted_by=ballot_num))
+            self.stop()
 
 
 class Commander(Role):
     """ perform the Accept/Accepted portion of the Multi-Paxos algorithm for a leader """
-    pass
+
+    def __init__(self, node, ballot_num, slot, proposal, peers):
+        super(Commander, self).__init__(node)
+        self.ballot_num = ballot_num
+        self.slot = slot
+        self.proposal = proposal
+        self.acceptors = set([])
+        self.peers = peers
+        self.quorum = len(peers) / 2 + 1
+
+    def start(self):
+        self.node.send(set(self.peers) - self.acceptors, Accept(
+            slot=self.slot, ballot_num=self.ballot_num, proposal=self.proposal))
+        self.set_timer(ACCEPT_RETRANSMIT, self.start)
+
+    def finished(self, ballot_num, preempted):
+        if preempted:
+            self.node.send([self.node.address],
+                           Preempted(slot=self.slot, preempted_by=ballot_num))
+        else:
+            self.node.send([self.node.address],
+                           Decided(slot=self.slot))
+        self.stop()
+
+    def do_accepted(self, sender, slot, ballot_num):
+        if slot != self.slot:
+            return
+        if ballot_num == self.ballot_num:
+            self.acceptors.add(sender)
+            if len(self.acceptors) < self.quorum:
+                return
+            self.node.send(self.peers, Decision(
+                slot=self.slot, proposal=self.proposal))
+            self.finished(ballot_num, False)
+        else:
+            self.finished(ballot_num, True)
 
 
 class Bootstrap(Role):
     """ introduce a new node to an existing cluster """
-    pass
+
+    def __init__(self, node, peers, execute_fn,
+                 replica_cls=Replica, acceptor_cls=Acceptor, leader_cls=Leader,
+                 commander_cls=Commander, scout_cls=Scout):
+        super(Bootstrap, self).__init__(node)
+        self.execute_fn = execute_fn
+        self.peers = peers
+        self.peers_cycle = itertools.cycle(peers)
+        self.replica_cls = replica_cls
+        self.acceptor_cls = acceptor_cls
+        self.leader_cls = leader_cls
+        self.commander_cls = commander_cls
+        self.scout_cls = scout_cls
+
+    def start(self):
+        self.join()
+
+    def join(self):
+        self.node.send([next(self.peers_cycle)], Join())
+        self.set_timer(JOIN_RETRANSMIT, self.join)
+
+    def do_welcome(self, sender, state, slot, decisions):
+        self.acceptor_cls(self.node)
+        self.replica_cls(self.node, execute_fn=self.execute_fn, peers=self.peers,
+                         state=state, slot=slot, decisions=decisions)
+        self.leader_cls(self.node, peers=self.peers, commander_cls=self.commander_cls,
+                        scout_cls=self.scout_cls).start()
+        self.stop()
 
 
 class Seed(Role):
     """ create a new cluster """
-    pass
+
+    def __init__(self, node, initial_state, execute_fn, peers,
+                 bootstrap_cls=Bootstrap):
+        super(Seed, self).__init__(node)
+        self.initial_state = initial_state
+        self.execute_fn = execute_fn
+        self.peers = peers
+        self.bootstrap_cls = bootstrap_cls
+        self.seen_peers = set([])
+        self.exit_timer = None
+
+    def do_join(self, sender):
+        self.seen_peers.add(sender)
+        if len(self.seen_peers) <= len(self.peers) / 2:
+            return
+
+        # cluster is ready - welcome everyone
+        self.node.send(list(self.seen_peers), Welcome(
+            state=self.initial_state, slot=1, decisions={}))
+
+        # stick around for long enough that we don't hear any new JOINs from
+        # the newly formed cluster
+        if self.exit_timer:
+            self.exit_timer.cancel()
+        self.exit_timer = self.set_timer(JOIN_RETRANSMIT * 2, self.finish)
+
+    def finish(self):
+        # bootstrap this node into the cluster we just seeded
+        bs = self.bootstrap_cls(self.node,
+                                peers=self.peers, execute_fn=self.execute_fn)
+        bs.start()
+        self.stop()
 
 
 class Requester(Role):
     """ request a distributed state machine operation """
-    pass
+
+    client_ids = itertools.count(start=100000)
+
+    def __init__(self, node, n, callback):
+        super(Requester, self).__init__(node)
+        self.client_id = self.client_ids.next()
+        self.n = n
+        self.output = None
+        self.callback = callback
+
+    def start(self):
+        self.node.send([self.node.address],
+                       Invoke(caller=self.node.address,
+                              client_id=self.client_id, input_value=self.n))
+        self.invoke_timer = self.set_timer(INVOKE_RETRANSMIT, self.start)
+
+    def do_Invoked(self, sender, client_id, output):
+        if client_id != self.client_id:
+            return
+        self.logger.debug("received output %r" % (output,))
+        self.invoke_timer.cancel()
+        self.callback(output)
+        self.stop()
